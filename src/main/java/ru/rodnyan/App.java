e0c9575e95e6d80098be41c94619c70d14f342eb
Rodnin Roman <rodninroma@gmail.com>
2018-10-30 07:22:39 +0300

    initial commit

---
+++ src/main/java/ru/rodnyan/App.java
@@ -0,0 +1,13 @@
+package ru.rodnyan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
